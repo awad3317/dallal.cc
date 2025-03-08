f7713810309c8731bc7148d7b0acbdbf75686ef8
awad3317 <awad3317@icloud.com>
2025-03-08 21:27:41 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\api\CommentController;
+use App\Http\Controllers\API\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AdController;
