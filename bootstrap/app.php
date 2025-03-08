82f387d2cd205334c1610f4873bcfa84918bacec
awad3317 <awad3317@icloud.com>
2025-03-08 13:46:34 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->group('api', [
-            \app\Http\Middleware\CorsMiddleware::class,
+            \App\Http\Middleware\CorsMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
