c986a82428137a0b67ee946aec3605b8d946c49d
awad3317 <awad3317@icloud.com>
2025-03-08 15:18:01 +0300

    update cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,8 +16,9 @@ class CorsMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $headers = [
-            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Origin' => 'http://localhost:3000',
             'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With',
         ];
         if ($request->isMethod('OPTIONS')) {
