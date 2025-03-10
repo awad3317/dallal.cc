9e78c8115921dfc71436dea37e9fa3e57961c9ec
awad3317 <awad3317@icloud.com>
2025-03-10 14:33:54 +0300

    Update UserDashboardController.php

--- app/Http/Controllers/API/Dashboard/UserDashboardController.php
+++ app/Http/Controllers/API/Dashboard/UserDashboardController.php
@@ -34,7 +34,7 @@ class UserDashboardController extends Controller
     {
         try {
             $user = $this->UserRepository->getById(Auth::id() ?? 1);// just in test
-            $ads = $user->ads->paginate(10); 
+            $ads = $user->ads()->paginate(10); 
             return ApiResponseClass::sendResponse($ads, 'ads retrieved successfully.');
         } catch (Exception $e) {
             return ApiResponseClass::sendError('Error retrieving ads: ' . $e->getMessage());
@@ -45,7 +45,7 @@ class UserDashboardController extends Controller
     {
         try {
             $user = $this->UserRepository->getById(Auth::id() ?? 1);// just in test
-            $favorites = $user->favorites->paginate(10); 
+            $favorites = $user->favorites()->paginate(10); 
             return ApiResponseClass::sendResponse($favorites, 'favorites retrieved successfully.');
         } catch (Exception $e) {
             return ApiResponseClass::sendError('Error retrieving favorites: ' . $e->getMessage());
