8869c6fdbdac7211d207d2c0912631554bf703cd
awad3317 <awad3317@icloud.com>
2025-03-18 18:39:58 +0300

    Update channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
@@ -7,5 +8,5 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('private-channel.user.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int) Auth::id() == (int) $id;
 });
