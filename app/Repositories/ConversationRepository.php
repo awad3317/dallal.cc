fa55b6a371ccd1963f365754db1d65a3b14aed67
awad3317 <awad3317@icloud.com>
2025-04-01 22:05:46 +0300

    Update ConversationRepository.php

--- app/Repositories/ConversationRepository.php
+++ app/Repositories/ConversationRepository.php
@@ -85,7 +85,7 @@ class ConversationRepository implements RepositoriesInterface
             ->orWhere('receiver_id', $userId)
             ->with(['sender','receiver','messages' => 
                 function($query) {
-                    $query->latest('sent_at')->limit(1);
+                    $query->orderBy('created_at', 'DESC')->limit(1);
                 },
                 'ad:id,title'
             ])
