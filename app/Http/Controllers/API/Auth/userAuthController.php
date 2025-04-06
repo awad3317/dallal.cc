0b9ea30425312f814d38473b979ae644a07268d1
awad3317 <awad3317@icloud.com>
2025-04-06 16:56:07 +0300

    Update userAuthController.php

--- app/Http/Controllers/API/Auth/userAuthController.php
+++ app/Http/Controllers/API/Auth/userAuthController.php
@@ -89,7 +89,7 @@ class userAuthController extends Controller
             // Send an email with the OTP code to the user's email address
             // SendOtpEmailJob::dispatch($user->email, $otp);
             Mail::to($user->email)->send(new OtpMail($otp));
-            return ApiResponseClass::sendError("'البريد الإلكتروني غير محقق. تم إرسال رمز التحقق'.$user->email", null,200);
+            return ApiResponseClass::sendError(['message'=>"'البريد الإلكتروني غير محقق. تم إرسال رمز التحقق'.$user->email",'OTP'=>'required'], null,200);
         }
 
         // Check if the user exists and if the password is correct
