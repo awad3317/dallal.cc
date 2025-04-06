e511d920b33843a2e9885ecd45cb9197770d00f7
awad3317 <awad3317@icloud.com>
2025-04-06 11:29:29 +0300

    Update OtpMail.php

--- app/Mail/OtpMail.php
+++ app/Mail/OtpMail.php
@@ -28,7 +28,7 @@ class OtpMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Your OTP Code for Account Creation',
+            subject: 'رمز التحقق الخاص بك',
         );
     }
 
