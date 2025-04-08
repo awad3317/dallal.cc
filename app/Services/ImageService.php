b3a93f49d6a83af35ebe8f06a02bb28da07c5e34
awad3317 <awad3317@icloud.com>
2025-04-08 22:40:36 +0300

    update

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -4,7 +4,8 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
+// use Intervention\Image\Facades\Image;
+use Buglinjo\LaravelWebp\Webp;
 
 class ImageService
 {
@@ -17,18 +18,30 @@ class ImageService
      */
     public function saveImage($image, $folder = 'Primary_images')
     {
-
-        // Generate a unique filename
-        $filename = uniqid('', true) . '.' . $image->getClientOriginalExtension();
+         // Generate a unique filename with .webp extension
+        $filename = uniqid('', true) . '.webp';
         $filePath = $folder . '/' . $filename;
-
-        // Save the file to the specified folder
-        $image->storeAs($folder, $filename, 'public');
-        // $filePath = Storage::putFileAs($folder, $image, $filename);
-
+    
+        // Convert and save as WebP
+        $webp = new Webp();
+        $tempPath = $image->path();
+        $webp->make($tempPath)
+         ->save(storage_path('app/public/' . $filePath), 80); // 80 = جودة الصورة (0-100)
+    
         // Return the full file path
         return 'storage/' . $filePath;
-        // return 'storage/' . $filename;
+
+        // Generate a unique filename
+        // $filename = uniqid('', true) . '.' . $image->getClientOriginalExtension();
+        // $filePath = $folder . '/' . $filename;
+
+        // // Save the file to the specified folder
+        // $image->storeAs($folder, $filename, 'public');
+        // // $filePath = Storage::putFileAs($folder, $image, $filename);
+
+        // // Return the full file path
+        // return 'storage/' . $filePath;
+        // // return 'storage/' . $filename;
     }
 
     public function deleteImage($image){
