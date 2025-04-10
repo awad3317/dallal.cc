c4507afa515a827b80a6df5973a8af4193de8fad
awad3317 <awad3317@icloud.com>
2025-04-10 12:49:21 +0300

    Update script.php

--- public/script.php
+++ public/script.php
@@ -7,16 +7,26 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-// الحصول على المستخدم ككائن فردي (ليس مجموعة)
-$user = User::where('id', 3)->first(['id', 'name', 'email']);
-
-if ($user) {
+$users = User::where('receive_email_notifications', '=',true)->get();
+if ($users->isEmpty()) {
+    echo "لا يوجد مستخدمون مفعلون لاستقبال الإشعارات البريدية\n";
+    exit;
+}
+foreach ($users as $user) {
     try {
         Mail::to($user->email)->send(new WelcomeMail($user));
         echo "تم إرسال رسالة ترحيبية إلى: {$user->email}\n";
     } catch (\Exception $e) {
-        echo "حدث خطأ أثناء الإرسال: " . $e->getMessage() . "\n";
+        echo "حدث خطأ أثناء الإرسال إلى {$user->email}: " . $e->getMessage() . "\n";
     }
-} else {
-    echo "لم يتم العثور على المستخدم ذو ID 3\n";
 }
+// if ($user) {
+//     try {
+//         Mail::to($user->email)->send(new WelcomeMail($user));
+//         echo "تم إرسال رسالة ترحيبية إلى: {$user->email}\n";
+//     } catch (\Exception $e) {
+//         echo "حدث خطأ أثناء الإرسال: " . $e->getMessage() . "\n";
+//     }
+// } else {
+//     echo "لم يتم العثور على المستخدم ذو ID 3\n";
+// }
