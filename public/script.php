52ddf023e8083b74d68eebeb96e6273bd503098e
awad3317 <awad3317@icloud.com>
2025-04-10 06:03:26 +0300

    add scrept

---
+++ public/script.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Mail;
+use App\Models\User;
+use App\Mail\WelcomeMail;
+// use App\Mail\NewMessagesNotification;
+
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+$users = User::where('id','=',3)->get();
+
+Mail::to($users->email)->send(new WelcomeMail($users));
