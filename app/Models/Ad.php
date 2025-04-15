bc09f07381d575490777be4406421bee15b1d733
awad3317 <awadaaa3317@gmail.com>
2025-04-15 12:09:35 +0300

    Update Ad.php

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -117,7 +117,7 @@ class Ad extends Model
      */
     public function comments()
     {
-        return $this->hasMany(Comment::class)->orderBy('created_at', 'asc');
+        return $this->hasMany(Comment::class)->orderBy('created_at', 'desc');
     }
 
     /**
